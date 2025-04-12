115fefe1d2d8c8a92df176086fed7009e0d89a3a
kajmaj87 <kajmaj87@gmail.com>
2025-04-12 13:29:29 +0200

    Added some more buttons

--- scripts/ui/input.lua
+++ scripts/ui/input.lua
@@ -1,14 +1,26 @@
 -- Register a keyboard shortcut
-ui.input.register_shortcut("SHIFT+X", function()
-	print("SHIFT+X was pressed!")
-end)
+local origin = ui.input.register_shortcut
+local shortcuts = {}
+ui.input.register_shortcut = function(combo, description, handler)
+    shortcuts[combo] = { description = description }
+    origin(combo, function()
+        print("Shortcut triggered:", combo, description)
+        handler()
+    end)
+end
 
-ui.input.register_shortcut("R", function()
-	print("x was pressed!")
+ui.input.register_shortcut("ESCAPE", "Exit mode", function()
+end)
+ui.input.register_shortcut("R", "Print R", function()
+end)
+ui.input.register_shortcut("F1", "Print keyboard shortcuts", function()
+    for combo, data in pairs(shortcuts) do
+        print("Shortcut:", combo, "Description:", data.description)
+    end
 end)
 -- Register a mouse click handler
 ui.input.register_mouse("LMB", function()
-	local x, y = ui.tile.hovered()
-	local person = api.person.create("testa", x, y)
-	print_compact("Person:", person)
+    local x, y = ui.tile.hovered()
+    local person = api.person.create("testa", x, y)
+    print_compact("Person:", person)
 end)
